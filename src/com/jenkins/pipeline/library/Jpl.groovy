742b1ce50a452cc1c299065afdd1e72918f0d23d
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-03-21 12:45:54 +0100

    Test gitPromote

--- src/com/jenkins/pipeline/library/Jpl.groovy
+++ src/com/jenkins/pipeline/library/Jpl.groovy
@@ -2,8 +2,14 @@ package com.jenkins.pipeline.library
 
 class Jpl {
     def gitPromoteCmd = "wget -O - https://raw.githubusercontent.com/pedroamador/git-promote/master/git-promote | bash -s -- "
+    def steps
+
+    Jpl (steps) {
+        this.steps = steps
+    }
+
     def gitPromote(upstream,downstream) {
-        sh "echo ${gitPromoteCmd}"
-        sh "${gitPromoteCmd} -m 'Merge from ${upstream} with Jenkins' ${downstream} quality"
+        steps.sh "echo ${gitPromoteCmd}"
+        steps.sh "${gitPromoteCmd} -m 'Merge from ${upstream} with Jenkins' ${downstream} quality"
     }
 }
