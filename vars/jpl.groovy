49eef2997597574d62f1c3dae7603a838b7467c0
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-03-21 11:52:59 +0100

    Add gitPromote

--- vars/jpl.groovy
+++ vars/jpl.groovy
@@ -1,14 +1,9 @@
 #!groovy
 
 class jpl implements Serializable {
-    private String name
-    def setName(value) {
-        name = value
-    }
-    def getName() {
-        name
-    }
-    def caution(message) {
-        println("Hello, ${name}! CAUTION: ${message}")
+    def gitPromoteCmd = "wget -O - https://raw.githubusercontent.com/pedroamador/git-promote/master/git-promote | bash -s -- "
+    def gitPromote(upstream,downstream) {
+        sh "echo ${gitPromoteCmd}"
+        sh "${gitPromoteCmd} -m 'Merge from ${upstream} with Jenkins' ${downstream} quality"
     }
 }
