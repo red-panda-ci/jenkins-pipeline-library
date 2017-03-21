c65bd210a662e9d1bb47bddec5daf2838b660b14
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-03-21 10:59:36 +0100

    Add jpl class

--- vars/jpl.groovy
+++ vars/jpl.groovy
@@ -1,5 +1,7 @@
 #!groovy
 
-def jpl_gitPromote = "wget -O - https://raw.githubusercontent.com/pedroamador/git-promote/master/git-promote | bash -s -- "
-def jpl_to = emailextrecipients([[$class: 'DevelopersRecipientProvider'],[$class: 'CulpritsRecipientProvider'],[$class: 'UpstreamComitterRecipientProvider'],[$class: 'FirstFailingBuildSuspectsRecipientProvider'],[$class: 'FailingTestSuspectsRecipientProvider']])
+class jpl {
+    def gitPromote = "wget -O - https://raw.githubusercontent.com/pedroamador/git-promote/master/git-promote | bash -s -- "
+    def to = emailextrecipients([[$class: 'DevelopersRecipientProvider'],[$class: 'CulpritsRecipientProvider'],[$class: 'UpstreamComitterRecipientProvider'],[$class: 'FirstFailingBuildSuspectsRecipientProvider'],[$class: 'FailingTestSuspectsRecipientProvider']])
+}
 
