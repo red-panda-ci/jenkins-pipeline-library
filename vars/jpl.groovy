8efcc8b54719281096f14485076e0c4a56553e70
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-03-21 11:07:15 +0100

    Add email notify

--- vars/jpl.groovy
+++ vars/jpl.groovy
@@ -1,7 +1,21 @@
 #!groovy
 
 class jpl {
-    def gitPromote = "wget -O - https://raw.githubusercontent.com/pedroamador/git-promote/master/git-promote | bash -s -- "
-    def to = emailextrecipients([[$class: 'DevelopersRecipientProvider'],[$class: 'CulpritsRecipientProvider'],[$class: 'UpstreamComitterRecipientProvider'],[$class: 'FirstFailingBuildSuspectsRecipientProvider'],[$class: 'FailingTestSuspectsRecipientProvider']])
-}
+    private array emailRecipients
+    private string emailCopy
 
+    def gitPromote () {
+        return "wget -O - https://raw.githubusercontent.com/pedroamador/git-promote/master/git-promote | bash -s -- "
+    }
+    def setEmailRecipients (value) {
+        emailRecipients = value
+
+    }
+    def setEmailCopy (value) {
+        emailCopy = value
+    }
+
+    def notifyEmail () {
+        mail to: emailRecipients, cc: emailCopy, subject: "Job ${env.JOB_NAME} [${env.BUILD_NUMBER}] finished with ${currentBuild.result}", body: "See ${env.BUILD_URL}/console"
+    }
+}
