b3fcc4d27752c2340a83f207fbcaa7dc7242547a
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-03-21 15:34:53 +0100

    Add sonar scanner class

---
+++ vars/jplSonarScanner.groovy
@@ -0,0 +1,23 @@
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    
+    // name => Sonar Scaner Tool Name
+    wrap ([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
+        script {
+            timeout(time: 1, unit: 'HOURS') {
+                sh 'export'
+                withSonarQubeEnv(config.name) {
+                    sh 'export'
+                    sh "${sonarHome}/bin/sonar-scanner"
+                }
+                def qg = waitForQualityGate()
+                if (qg.status != 'OK') {
+                    error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                }
+            }
+        }
+    }
+}
