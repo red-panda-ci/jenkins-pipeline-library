39aefa4ec354b15fa30d1b51a494f6850b75cc9f
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-03-23 06:26:18 +0100

    Change function sonar

--- vars/jplSonarScanner.groovy
+++ vars/jplSonarScanner.groovy
@@ -1,15 +1,9 @@
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    
-    // name => Sonar Scaner Tool Name
+def call(String sonarScannerToolName = "SonarQube" {
     wrap ([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
         script {
             timeout(time: 1, unit: 'HOURS') {
-                def sonarHome = tool 'SonarQube';
-                withSonarQubeEnv(config.sonarScannerToolName) {
+                def sonarHome = tool sonarScannerToolName;
+                withSonarQubeEnv(sonarScannerToolName) {
                     sh "${sonarHome}/bin/sonar-scanner"
                 }
                 def qg = waitForQualityGate()
