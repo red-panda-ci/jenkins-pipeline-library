0a2e2542828d7c6f39ba665f9d291903e428db80
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-03-23 06:47:19 +0100

    Fix git promote bug

--- vars/jplPromote.groovy
+++ vars/jplPromote.groovy
@@ -1,11 +1,10 @@
-// Global settings
-def gitPromote = "wget -O - https://raw.githubusercontent.com/pedroamador/git-promote/master/git-promote | bash -s -- "
-
 /**
  Promote code on release
 
  Merge code from upstream branch to downstream branch, then make "push" to the repository
  When the code is in the repository, launch the job and wait for result
+
+ The function is using "git promote" script
 
  */
 def call(String upstreamBranch,String downstreamBranch,String jobName) {    
@@ -15,6 +14,6 @@ def call(String upstreamBranch,String downstreamBranch,String jobName) {
     }
     checkout scm
     sh 'git submodule update --init'
-    sh "${gitPromote} -m 'Merge from ${upstreamBranch} with Jenkins' ${upstreamBranch} ${downstreamBranch}"
+    sh "wget -O - https://raw.githubusercontent.com/pedroamador/git-promote/master/git-promote | bash -s -- 'Merge from ${upstreamBranch} with Jenkins' ${upstreamBranch} ${downstreamBranch}"
     build (job: jobName, wait: true)
 }
