9958c94c8acde6c2a255dbcefd48659a0d3e1a45
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-06-18 18:59:08 +0200

    Don't get repository code in APK build function

--- vars/jplBuildAPK.groovy
+++ vars/jplBuildAPK.groovy
@@ -10,7 +10,6 @@
 def call(String lane,String versionSuffix = '') {
     timestamps {
         ansiColor('xterm') {
-            jplCheckoutSCM()
             sh "mkdir -p ?/.android && cp -n ~/.android/debug.keystore ?/.android"
             sh "fastlane ${lane} versionSuffi:${versionSuffix}"
         }
