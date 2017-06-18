3f91bab6b1e580a6afcf1ca685e5947b3e4f4093
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-06-18 11:40:02 +0200

    Add jplCheckoutSCM function

---
+++ vars/jplChechoutSCM.groovy
@@ -0,0 +1,16 @@
+/**
+
+  Checkout SCM
+
+  Get the code from SCM and init / update submodules
+  Leave the repository on the actual branch, instead of "deatached"
+
+*/
+def call() {
+    timestamps {
+        ansiColor('xterm') {
+            checkout scm
+            sh 'git checkout ' + env.BRANCH_NAME + ' && git pull && git submodule update --init'
+        }
+    }
+}
