43617d53b9a55200c74419ce0fc59b4ef4dc9bc0
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-06-19 10:05:51 +0200

    Export test

--- vars/jplConfig.groovy
+++ vars/jplConfig.groovy
@@ -23,7 +23,8 @@ class jplConfig implements Serializable {
 
     // Constructor
     jplConfig () {
-        this.laneName = (env.BRANCH_NAME == "master") ? '' :  env.BUILD_NUMBER + "-" + env.BRANCH_NAME.tokenize("/")[0]
-        this.versionSuffix = ((env.BRANCH_NAME in "staging,quality,master") || env.BRANCH_NAME.startsWith('release/')) ? env.BRANCH_NAME.tokenize("/")[0] : 'develop'
+        sh "export"
+        //this.laneName = (env.BRANCH_NAME == "master") ? '' :  env.BUILD_NUMBER + "-" + env.BRANCH_NAME.tokenize("/")[0]
+        //this.versionSuffix = ((env.BRANCH_NAME in "staging,quality,master") || env.BRANCH_NAME.startsWith('release/')) ? env.BRANCH_NAME.tokenize("/")[0] : 'develop'
     }
 }
