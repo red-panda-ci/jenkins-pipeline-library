dbd0fd5b74328a096a5102f1f6300e3c171d2aa2
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-06-19 12:10:38 +0200

    Use jplConfig in jplBuildAPK

--- vars/jplBuildAPK.groovy
+++ vars/jplBuildAPK.groovy
@@ -3,14 +3,26 @@
   Build APK with Fastlane within docker into Jenkins
 
   Parameters:
-  * String lane What lane of Fastlane is going to be used in the build
+  * String laneName What is the lane of Fastlane that's going to be used in the build
   * String versionSuffix Version suffix
 
 */
-def call(String lane,String versionSuffix = '') {
+def call(String laneName = '',String versionSuffix = '') {
+    if (lane == '') {
+        buildLaneName = jplConfig.laneNAme
+    }
+    else {
+        buildVersionSuffix = laneName
+    }
+    if (versionSuffix == '') {
+        buildVersionSuffix = jplConfig.versionSuffix
+    }
+    else {
+        buildVersionSuffix = versionSuffix
+    }
     timestamps {
         ansiColor('xterm') {
-            sh "fastlane ${lane} versionSuffi:${versionSuffix}"
+            sh "fastlane ${buildLaneName} versionSuffix:${buildVersionSuffix}"
         }
     }
 }
