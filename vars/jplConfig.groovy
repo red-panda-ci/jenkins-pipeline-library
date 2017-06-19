123e18e55a5c1cd335fa4355bd0b9b0bc1bd59c8
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-06-19 11:46:49 +0200

    Test

--- vars/jplConfig.groovy
+++ vars/jplConfig.groovy
@@ -20,4 +20,15 @@ class jplConfig implements Serializable {
     def getVersionSuffix() {
         return this.versionSuffix
     }
+
+    def initialize(env) {
+        if (env.BRANCH_NAME == null) {
+            branchName = 'develop'
+        }
+        else {
+            branchName = env.BRANCH_NAME
+        }
+        this.laneName = ((branchName in "staging,quality,master") || branchName.startsWith('release/')) ? branchName.tokenize("/")[0] : 'develop'
+        this.versionSuffix = (branchName == "master") ? '' :  env.BUILD_NUMBER + "-" + branchName.tokenize("/")[0]
+    }
 }
