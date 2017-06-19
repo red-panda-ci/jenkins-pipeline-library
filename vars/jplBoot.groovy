f5b57d7e8a28e46745ee98f70d3411d26ce47739
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-06-19 11:29:57 +0200

    Re-add config & boot

---
+++ vars/jplBoot.groovy
@@ -0,0 +1,24 @@
+/**
+
+  Initialize jpl
+
+*/
+def call(String laneName = '',String versionSuffix = '') {
+    if (env.BRANCH_NAME == null) {
+        branchName = 'develop'
+    }
+    else {
+        branchName = env.BRANCH_NAME
+    }
+    if (laneName == '') {
+        laneName = ((branchName in "staging,quality,master") || branchName.startsWith('release/')) ? branchName.tokenize("/")[0] : 'develop'
+    }
+    if (versionSuffix == '') {
+        versionSuffix = (branchName == "master") ? '' :  env.BUILD_NUMBER + "-" + branchName.tokenize("/")[0]
+    }
+    jplConfig.laneName = laneName
+    jplConfig.versionSuffix = versionSuffix
+    echo "LANE name: " + jplConfig.laneName
+    echo 'VERSION suffix: ' + jplConfig.versionSuffix
+    
+}
---
+++ vars/jplConfig.groovy
@@ -0,0 +1,19 @@
+/**
+
+  Global config variables
+
+*/
+def setLaneName(value) {
+    laneName = value
+}
+def getLaneName() {
+    laneName
+}
+
+def setVersionSuffix(value) {
+    versionSuffix = value
+}
+def getVersionSuffix() {
+    versionSuffix
+}
+
