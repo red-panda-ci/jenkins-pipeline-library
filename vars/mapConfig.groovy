c99205cf4f24eb799d0904ad516649ee443f33cb
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-06-25 13:05:33 +0200

    mapConfig work

--- vars/mapConfig.groovy
+++ vars/mapConfig.groovy
@@ -8,6 +8,6 @@ public String projectName
 
 def call () {
     echo env.JOB_NAME
-    echo cfg.projectName
-    echo cfg.targetPlatform
+    echo jplConfig.projectName
+    echo jplConfig.targetPlatform
 }
