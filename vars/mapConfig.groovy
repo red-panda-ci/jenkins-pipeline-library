a0b466b15967beb79fbb92b8cec03653a6737c26
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-06-25 12:49:18 +0200

    MapConfig test

--- vars/mapConfig.groovy
+++ vars/mapConfig.groovy
@@ -7,8 +7,6 @@ public String env
 public String projectName
 
 def call () {
-    this.env = env
-    this.projectName = 'testProject'
-    echo 'Project config done'
-    return this
+    echo jplConfig.projectName
+    echo jplConfig.targetPlatform
 }
