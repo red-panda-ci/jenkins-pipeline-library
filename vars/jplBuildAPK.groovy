3c2acbbaa5ec32d64d685d79b9136aeaa421ec8e
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-06-25 17:20:56 +0200

    Add command parameter to builApk

--- vars/jplBuildAPK.groovy
+++ vars/jplBuildAPK.groovy
@@ -6,10 +6,13 @@
   * jplConfig project config class
 
 */
-def call(jplConfig) {
+def call(jplConfig,command='') {
+    if (command == '') {
+        command="fastlane ${jplConfig.laneName} versionSuffix:${jplConfig.versionSuffix}"
+    }
     timestamps {
         ansiColor('xterm') {
-            sh "ci-scripts/.jenkins_library/bin/buildApk.sh --sdkVersion=${jplConfig.projectName} --command='fastlane ${jplConfig.laneName} versionSuffix:${jplConfig.versionSuffix}'"
+            sh "ci-scripts/.jenkins_library/bin/buildApk.sh --sdkVersion=${jplConfig.projectName} --command='${command}'"
             script {
                 archive '**/*DebugUnitTest.exec'
                 if (!env.BRANCH_NAME.startsWith('PR-')) {
