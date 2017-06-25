efdc3efd9f5c1f239e531df399bd7a1cdbcd108c
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-06-25 15:48:09 +0200

    Fix buildApk typo

--- vars/jplBuildAPK.groovy
+++ vars/jplBuildAPK.groovy
@@ -9,7 +9,7 @@
 def call(jplConfig) {
     timestamps {
         ansiColor('xterm') {
-            sh "ci-scripts/.jenkins_library/bin/buildApk.sh --sdkVerison=${jplConfig.projectName} --command='fastlane ${jplConfig.laneName} versionSuffix:${jplConfig.versionSuffix}'"
+            sh "ci-scripts/.jenkins_library/bin/buildApk.sh --sdkVersion=${jplConfig.projectName} --command='fastlane ${jplConfig.laneName} versionSuffix:${jplConfig.versionSuffix}'"
             script {
                 archive '**/*DebugUnitTest.exec'
                 if (!env.BRANCH_NAME.startsWith('PR-')) {
