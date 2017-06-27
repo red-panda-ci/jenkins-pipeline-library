5e52bae8db81763242e2429a3b2b613a7689aace
Pedro Amador <pedro.amador@gigigo.com>
2017-06-27 20:49:30 +0200

    Test post build

--- vars/jplPostBuild.groovy
+++ vars/jplPostBuild.groovy
@@ -38,7 +38,6 @@ def call(cfg) {
                                               issuetype: [id: '1']  // type 1: bug
                                               ]]
                     response = jiraNewIssue issue: issueData
-                    echo "New JIRA issue opened: " + response.data.toString()
                 }
             }
         }
