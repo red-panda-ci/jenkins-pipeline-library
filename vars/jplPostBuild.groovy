b96985f134313482dba9fddca4daec779ba439d1
Pedro Amador <pedro.amador@gigigo.com>
2017-06-27 20:17:16 +0200

    Test post build

--- vars/jplPostBuild.groovy
+++ vars/jplPostBuild.groovy
@@ -31,15 +31,14 @@ def call(cfg) {
                 // Open JIRA tickets on 'NOT SUCCESS build'
                 echo "Result status: ${resultStatus}, cfg.jiraProjectKey: ${cfg.jiraProjectKey}"
                 if ((resultStatus != 'SUCCESS') && (cfg.jiraProjectKey != '')) {
-                    /*def issueData = [fields: [
+                    def issueData = [fields: [
                                               project: [key: cfg.jiraProjectKey,
                                               summary: "Job [${env.JOB_NAME}] [#${env.BUILD_NUMBER}] finished with ${resultStatus}${branchInfo}",
                                               description: "View details on ${env.BUILD_URL}/console",
-                                              issuetype: [id: '1'], // type 1: bug,
+                                              issuetype: [id: '1']  // type 1: bug
                                               ]]
                     response = jiraNewIssue issue: issueData
-                    echo "New JIRA issue opened: ${response.successful.toString()}"*/
-                    echo 'finish with fail'
+                    echo "New JIRA issue opened: ${response.successful.toString()}"
                 }
             }
         }
