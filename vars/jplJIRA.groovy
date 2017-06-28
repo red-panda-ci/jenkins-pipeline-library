9f9ce5caaa0e7ca1c2f5ac5d54aa533747604a26
Pedro Amador <pedro.amador@gigigo.com>
2017-06-28 18:59:08 +0200

    Test access env variable on function

--- vars/jplJIRA.groovy
+++ vars/jplJIRA.groovy
@@ -26,6 +26,7 @@ def call(cfg) {
 def checkProjectExists(cfg) {
     if (cfg.jira.projectKey != '') {
         script {
+            println env
             // Look at https://jenkinsci.github.io/jira-steps-plugin/jira_get_project.html for more info
             def jiraProject = jiraGetProject idOrKey: cfg.jira.projectKey
             cfg.jira.projectData = jiraProject
