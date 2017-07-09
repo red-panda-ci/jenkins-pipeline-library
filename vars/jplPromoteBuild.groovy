580fae5be9d955318dfa6ff874061e0e3f25b5d2
Pedro Amador <pedro.amador@gigigo.com>
2017-07-09 14:34:02 +0200

    jplPromoteBuild default timeout catch

--- vars/jplPromoteBuild.groovy
+++ vars/jplPromoteBuild.groovy
@@ -11,11 +11,16 @@
 def call(cfg, message = "Promote Build", description = "Check to promote the build, leave uncheck to finish the build without promote") {
     timestamps {
         ansiColor('xterm') {
-            timeout(time: 8, unit: 'HOURS') {
-                cfg.promoteBuild = input(
-                    id: 'promoteBuild', message: message, 
-                    parameters: [[$class: 'BooleanParameterDefinition', defaultValue: false, description: description, name: 'promoteBuild']]
-                )
+            try {
+                cfg.promoteBuild = false
+                timeout(time: 30, unit: 'SECONDS') {
+                    cfg.promoteBuild = input(
+                        id: 'promoteBuild', message: message,
+                        parameters: [[$class: 'BooleanParameterDefinition', defaultValue: false, description: description, name: 'promoteBuild']]
+                    )
+                }
+            } catch(err) { // timeout reached or input false
+                echo "Don't promote this build"
             }
         }
     }
