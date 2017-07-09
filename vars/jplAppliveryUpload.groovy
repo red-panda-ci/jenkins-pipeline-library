c6cb87cc1449705bd1cd175eda7c6d21bfc27106
Pedro Amador <pedro.amador@gigigo.com>
2017-07-09 13:18:39 +0200

    Avoid use of reserved word package

--- vars/jplAppliveryUpload.groovy
+++ vars/jplAppliveryUpload.groovy
@@ -4,7 +4,7 @@
 
   Parameters:
   * cfg jplConfig class object
-  * String package File name to upload
+  * String packageFile File name to upload
   * String app App id
   * String token Applivery account token
 
@@ -13,7 +13,7 @@
   * cfg.versionSuffix
   
 */
-def call(cfg,package,app='',token='') {
+def call(cfg,String packageFile,String app='',String token='') {
     timestamps {
         ansiColor('xterm') {
             script {
@@ -24,7 +24,7 @@ def call(cfg,package,app='',token='') {
                         --apikey="${token}" \
                         --app="${app}" \
                         --os="${os}" \
-                        --package="${package}" \
+                        --package="${packageFile}" \
                         --versionName="${cfg.versionSuffix}" \
                         --notes="branch:${BRANCH_NAME}, build: #${BUILD_NUMBER}" \
                         --notify=${cfg.applivery.notify} \
