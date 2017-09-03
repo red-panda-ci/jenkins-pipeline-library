7f3bbc8363a8e54b02dc6257824b13c411425dbe
Pedro Amador <pedro.amador@gigigo.com>
2017-09-03 18:33:23 +0200

    Add Integration Events @ie functionality

--- vars/jplBuildAPK.groovy
+++ vars/jplBuildAPK.groovy
@@ -4,26 +4,48 @@
 
   Parameters:
   * cfg jplConfig class object
+  * string command What is the command to be executed in the build
+    Example: "./gradlew clean assembleDebug"
 
   cfg usage:
   * cfg.projectName
   * cfg.laneName
   * cfg.versionSuffix
 
+  Notes:
+  * Marked as DEPRECATED by jplBuild on 2017-09-02. Removed on a future release.
+
 */
-def call(cfg,command='') {
-    if (command == '') {
-        command="fastlane ${cfg.laneName} versionSuffix:${cfg.versionSuffix}"
-    }
+def call(cfg,String command='') {
     timestamps {
         ansiColor('xterm') {
-            sh "ci-scripts/.jenkins_library/bin/buildApk.sh --sdkVersion=${cfg.projectName} --command='${command}'"
-            script {
-                archiveArtifacts artifacts: '**/*DebugUnitTest.exec', fingerprint: true, allowEmptyArchive: true
-                if (!cfg.BRANCH_NAME.startsWith('PR-')) {
-                    archiveArtifacts artifacts: cfg.archivePattern, fingerprint: true, allowEmptyArchive: true
+            // Build default
+            if (cfg.ie.commandName == "fastlane") {
+                for (int i = 0; i < cfg.ie.parameter.size(); i++) {
+                    parameter = cfg.ie.parameter[i]
+                    command = "fastlane ${parameter.name} versionSuffix:${cfg.versionSuffix}"
+                    for (int j = 0; j < parameter.option.size(); j++) {
+                        option = parameter.option[j]
+                        command = "${command} ${option.name}:${option.status}"
+                    }
+                    print "Execute @ie on lane '${parameter.name}' with command '${command}'"
+                    this.buildAPK(cfg,command)
                 }
             }
+            else {
+                if (command == '') {
+                    command = "fastlane ${cfg.laneName} versionSuffix:${cfg.versionSuffix}"
+                }
+                this.buildAPK(cfg,command)
+            }
         }
+    }
+}
+
+def buildAPK(cfg,command) {
+    sh "ci-scripts/.jenkins_library/bin/buildApk.sh --sdkVersion=${cfg.projectName} --command='${command}'"
+    archiveArtifacts artifacts: '**/*DebugUnitTest.exec', fingerprint: true, allowEmptyArchive: true
+    if (!cfg.BRANCH_NAME.startsWith('PR-')) {
+        archiveArtifacts artifacts: cfg.archivePattern, fingerprint: true, allowEmptyArchive: true
     }
 }
--- vars/jplCheckoutSCM.groovy
+++ vars/jplCheckoutSCM.groovy
@@ -37,6 +37,7 @@ def call(cfg,repository='',branch='') {
                     script: "git describe --abbrev=0 --tags||echo ''",
                     returnStdout: true
                 ).trim()
+                jplIE(cfg)
                 sh 'git submodule update --init'
                 if (cfg.targetPlatform == 'android') {
                     sh "rm -rf ci-scripts/.jenkins_library && mkdir -p ci-scripts/.temp && cd ci-scripts/.temp/ && wget -q -O - https://github.com/pedroamador/ci-scripts/archive/master.zip | jar xvf /dev/stdin > /dev/null && chmod +x ci-scripts-master/bin/*.sh && mv ci-scripts-master ../.jenkins_library"
