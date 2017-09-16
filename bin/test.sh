17774351d45652b67ba64b3c2a864c2ab477d006
Pedro Amador <pedro.amador@gigigo.com>
2017-09-16 19:26:43 +0200

    New: Add first test using jenkins-dind

---
+++ bin/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd $(dirname "$0")/..
+
+echo -n "# Start jenkins as a daemon container"
+id=$(docker run -v `pwd`:/tmp/jenkins-pipeline-library --rm -d --privileged -p 8080:8080 redpandaci/jenkins-dind)
+echo " with id ${id}"
+
+echo "# Copy jenkins configuration"
+docker cp test/config/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml ${id}:/root/.jenkins
+docker cp test/jobs/ ${id}:/root/.jenkins/jobs/
+
+echo "# Wait for jenkins service to be initialized"
+sleep 10
+docker exec ${id} curl --max-time 50 --retry 10 --retry-delay 5 --retry-max-time 32 http://localhost:8080 -s > /dev/null
+
+echo "# Download jenkins cli"
+docker exec ${id} wget http://localhost:8080/jnlpJars/jenkins-cli.jar > /dev/null
+
+echo "# Execute the test"
+docker exec ${id} java -jar jenkins-cli.jar -s http://localhost:8080 build jplCheckoutSCM --username redpanda --password redpanda -s
+
+echo "# Destroy jenkins daemon container"
+docker rm -f ${id}
