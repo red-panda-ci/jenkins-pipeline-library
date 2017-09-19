10647fb722b9c318091cb002a06507ed393625cc
Pedro Amador <pedro.amador@gigigo.com>
2017-09-19 14:58:04 +0200

    Update: Delete generated changelog report file (Closes #7)

--- vars/jplCheckoutSCM.groovy
+++ vars/jplCheckoutSCM.groovy
@@ -66,6 +66,7 @@ def call(cfg, repository = '', branch = '') {
                     .replace('git@bitbucket.org:','https://bitbucket.org/')
                     sh "mkdir -p ci-scripts/reports && git log --format='%B%n-hash-%n%H%n-gitTags-%n%d%n-committerDate-%n%ci%n------------ _¿? ------------' HEAD --no-merges | docker run --rm -i -e COMMIT_DELIMITER='------------ _¿? ------------' -e PRESET='eslint' -e GIT_URL='${repositoryUrl}' -e FORMAT='html' madoos/node-pipe-changelog-generator > ci-scripts/reports/CHANGELOG.html"
                     archiveArtifacts artifacts: 'ci-scripts/reports/CHANGELOG.html', fingerprint: true, allowEmptyArchive: false
+                    fileDeleteOperation('ci-scripts/reports/CHANGELOG.html','')
 
                     // publish html
                     // snippet generator doesn't include "target:"
