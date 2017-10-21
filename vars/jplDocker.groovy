b6d0627804e9bf71b5e522172db51043e958d8a2
Pedro Amador <pedro.amador@gigigo.com>
2017-10-21 17:20:46 +0200

    New: Add jplDocker helper

---
+++ vars/jplDocker.groovy
@@ -0,0 +1,26 @@
+#!groovy
+/**
+
+  Docker image build & upload to registry
+
+  Parameters:
+  * cfg jplConfig class object
+  * String dockerImageName Name of the docker image
+  * String dockerRegistryURL The URL of the docker registry. Defaults to https://registry.hub.docker.com
+  * String dockerRegistryJenkinsCredentials Jenkins credentials for the docker registry
+  * String dockerfilePath The path where the Dockerfile is placed, default to the root path of the repository
+
+*/
+def call(cfg, String dockerImageName, String dockerRegitryURL = '', String dockerRegistryJenkinsCredentials = '', String dockerfilePath = "./") {
+
+    script {
+        def app
+        dockerRegistryURL = dockerRegistryURL == '' ? 'https://registry.hub.docker.com' : dockerRegistryURL
+        dir(dockerfilePath) {
+            app = docker.build(dockerImageName)
+        }
+        docker.withRegistry(dockerRegistryURL, dockerRegistryJenkinsCredentials) {
+            app.push("latest")
+        }
+    }
+}
