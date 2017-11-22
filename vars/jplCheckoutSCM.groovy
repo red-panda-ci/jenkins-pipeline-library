073540541071f326c4908f7b104c6bd1b0835817
Pedro Amador <pedro.amador@gigigo.com>
2017-11-22 23:03:13 +0100

    Update: Avoid to do a pull in jplCheckoutSCM

--- vars/jplCheckoutSCM.groovy
+++ vars/jplCheckoutSCM.groovy
@@ -1,5 +1,5 @@
 /**
-Get the code from SCM and init / update submodules
+Get the code from SCM and init
 Leave the repository on the actual branch, instead of "deatached"
 
 Parameters:
@@ -16,7 +16,7 @@ def call(cfg) {
         if (cfg.repository.url == '') {
             checkout scm
             if (!cfg.BRANCH_NAME.startsWith('PR-')) {
-                sh 'git checkout ' + cfg.BRANCH_NAME + ' && git pull '
+                sh 'git checkout ' + cfg.BRANCH_NAME
             }
         }
         else {
