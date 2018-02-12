7d27a6fee647eb6e06375ed1bd889b5228771978
Pedro Amador <pedro.amador@gigigo.com>
2018-02-12 10:16:55 +0100

    New: Agent attachment

--- bin/test.sh
+++ bin/test.sh
@@ -6,7 +6,7 @@
 # Functions
 function runWithinDocker () {
     command=$1
-    docker exec ${id} bash -c "${command}"
+    docker-compose exec -T jenkins-dind bash -c "${command}"
     returnValue=$((returnValue + $?))
 }
 
@@ -24,7 +24,7 @@ function runTest () {
         echo -e "\t\t(Mock)"
         return 0
     fi
-    docker exec ${id} bash -c "java -jar jenkins-cli.jar -s http://localhost:8080 build ${testName} --username redpanda --password redpanda -s"
+    docker-compose exec -T jenkins-dind bash -c "java -jar jenkins-cli.jar -s http://localhost:8080 build ${testName} --username redpanda --password redpanda -s"
     if [[ "$?" -ne "${expectedResult}" ]]
     then
         returnValue=$((returnValue + 1))
@@ -57,23 +57,18 @@ fi
 
 # Main
 echo "# Start jenkins as a docker-compose daemon"
-docker volume create jpl-dind-cache
-returnValue=$((returnValue + $?))
 docker-compose up -d --force-recreate
 returnValue=$((returnValue + $?))
 id=$(docker-compose ps -q jenkins-dind)
 returnValue=$((returnValue + $?))
 echo "# Started platform with id ${id} and port $(docker-compose port jenkins-dind 8080)"
 
-echo "# Copy jenkins configuration"
-docker cp test/config/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml ${id}:/root/.jenkins
-returnValue=$((returnValue + $?))
-docker cp test/jobs/ ${id}:/root/.jenkins/jobs/
-returnValue=$((returnValue + $?))
-
-echo "# Preparing jpl code for testing"
-docker cp `pwd` ${id}:/tmp/jenkins-pipeline-library
-returnValue=$((returnValue + $?))
+echo "# Copy jenkins configuration and prepare code for testing"
+docker-compose exec -T jenkins-dind rsync -a /tmp/jpl-source/test/jobs/ /root/.jenkins/jobs/
+docker-compose exec -T jenkins-dind cp /tmp/jpl-source/test/config/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml /root/.jenkins
+docker-compose exec -T jenkins-dind cp -R /tmp/jpl-source/ /tmp/jenkins-pipeline-library/
+docker-compose exec -T jenkins-agent1 cp -R /tmp/jpl-source/ /tmp/jenkins-pipeline-library/
+docker-compose exec -T jenkins-agent2 cp -R /tmp/jpl-source/ /tmp/jenkins-pipeline-library/
 runWithinDocker "rm -f /tmp/jenkins-pipeline-library/.git/hooks/* && git config --global push.default simple && git config --global user.email 'redpandaci@gmail.com' && git config --global user.name 'Red Panda CI'"
 if [[ "$1" == "local" ]] && [[ "$(git status --porcelain)" != "" ]]
 then
@@ -84,11 +79,21 @@ runWithinDocker "cd /tmp/jenkins-pipeline-library && git rev-parse --verify deve
 runWithinDocker "cd /tmp/jenkins-pipeline-library && git rev-parse --verify master || git checkout -b master"
 runWithinDocker "cd /tmp/jenkins-pipeline-library && git checkout -b 'release/v9.9.9' && git checkout -b 'hotfix/v9.9.9-hotfix-1' && git checkout -b 'jpl-test-promoted' && git checkout -b 'jpl-test' && git checkout `git rev-parse HEAD` > /dev/null 2>&1"
 
-echo "# Wait for jenkins service to be initialized"
+echo "# Waiting for jenkins service to be initialized"
 runWithinDocker "sleep 10 && curl --max-time 50 --retry 10 --retry-delay 5 --retry-max-time 32 http://localhost:8080 -s > /dev/null; sleep 10"
 
 echo "# Download jenkins cli"
 runWithinDocker "wget http://localhost:8080/jnlpJars/jenkins-cli.jar -q > /dev/null"
+
+echo "# Prepare agents"
+for agent in agent1 agent2
+do
+    secret=$(docker-compose exec -T jenkins-dind /tmp/jpl-source/bin/prepare_agent.sh ${agent})
+    docker-compose exec -d -T jenkins-${agent} jenkins-slave -url http://jenkins-dind:8080 ${secret} ${agent}
+done
+
+echo "# Reload Jenkins configuration"
+runWithinDocker "java -jar jenkins-cli.jar -s http://localhost:8080 reload-configuration --username redpanda --password redpanda"
 
 # Run tests
 if [[ ${doTests} == "true" ]]
@@ -110,7 +115,7 @@ fi
 if [[ "$1" != "local" ]]
 then
     echo "# Switch down and clear compose"
-    docker-compose down
+    docker-compose down -v
     returnValue=$((returnValue + $?))
 fi
 
