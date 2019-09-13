bcfd44cf9bdfde6f5a6b79aefab0ebd47a640608
Pedro Amador <ubuntu@slave.local>
2019-09-13 05:25:57 +0000

    Update: Allow slave connections on port 5000

--- test/bin/test-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p /var/jenkins_home/.jenkins
-install-plugins.sh < /opt/jpl-source/test/plugins.txt 
-cp -p /opt/jpl-source/test/jenkins_home/config.xml /var/jenkins_home/.jenkins
-cp -p /opt/jpl-source/test/config/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml /var/jenkins_home/.jenkins
-cp -Rp /opt/jpl-source/test/jobs/ /var/jenkins_home/.jenkins/jobs/
-chown jenkins:jenkins /var/jenkins_home -R
-/sbin/tini -- /usr/local/bin/teecke-jenkins-dind.sh
